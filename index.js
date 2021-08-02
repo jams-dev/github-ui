1197a7456b6921372c7027dc640e0181136c83ec
Davy Engone <davy@hackages.io>
2021-08-02 10:57:46 +0200

    done with google auth

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import {davy} from "./davy";
 import {nils} from "./nils";
-import {mike} from './mike';
+import {mike} from "./mike";
+import {google} from "./google";
 
 console.log("good morning", nils);
-console.log('good morning', mike);
-console.log('good morning', davy);
+console.log("good morning", mike);
+console.log("good morning", davy);
 
-function first(collection){
-    return collection[0];
+function first(collection) {
+  return collection[0];
 }
-
