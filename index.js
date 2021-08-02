9060a3812c2db328af799afd69d0317351770075
nils govaerts <nils.govaerts@gmail.com>
2021-08-02 09:54:57 +0200

    added nils.js and moddified index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {davy} from "./davy";
 import {nils} from "./nils";
 
-console.log("good morning", davy);
+console.log("good morning", nils);
 
 function first(collection) {
   return collection[0];
